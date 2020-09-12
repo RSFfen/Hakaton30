28d20f4d3f709f9a2671837ee31f1bb62e528174
RSFfen <rsf61.fen@gmail.com>
2020-09-12 14:27:54 +0300

    New data

---
+++ children30/children30/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('pupils/', include('pupils.urls')),
+    path('admin/', admin.site.urls),
+]
